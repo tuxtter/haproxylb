7d607d8f60d370707a6d57dd7ae3f3937b90b140
tuxtter <tuxtter@gmail.com>
2018-08-09 18:45:41 -0500

    Update WebNode1.sh

--- WebNode1.sh
+++ WebNode1.sh
@@ -7,21 +7,23 @@ yum upgrade -y
 yum install httpd net-tools epel-release wget rsync php php-mysql -y
 service httpd start
 chkconfig httpd on
+systemctl enable httpd.service
+systemctl enable rpcbind
 firewall-cmd --permanent --zone=public --add-port=80/tcp
-firewall-cmd --permanent --zone=public --add-port=4444/tcp
-firewall-cmd --permanent --zone=public --add-port=4567/tcp
-firewall-cmd --permanent --zone=public --add-port=4568/tcp
+#firewall-cmd --permanent --zone=public --add-port=4444/tcp
+#firewall-cmd --permanent --zone=public --add-port=4567/tcp
+#firewall-cmd --permanent --zone=public --add-port=4568/tcp
 firewall-cmd --zone=public --add-port=24007-24008/tcp --permanent
 firewall-cmd --zone=public --add-port=49152/tcp --permanent
 firewall-cmd --reload
 echo "<html><head><title>Nodo1</title></head><body><h1>Nodo1</h1></body></html>" > /var/www/html/index.html
-systemctl enable httpd.service
-sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
+sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config  && setenforce 0
+echo "ALL: 192.168.183.0/24 127.0.0.1/32" > /etc/hosts.allow
 yum -y install centos-release-gluster
 yum -y install glusterfs glusterfs-fuse glusterfs-server
 systemctl start glusterd
 systemctl enable glusterd
-systemctl status glusterd
-echo "192.168.183.4 gluster1" >> /etc/hosts
-echo "192.168.183.5 gluster2" >> /etc/hosts
-echo "192.168.183.6 gluster3" >> /etc/hosts
+echo "localhost:/vol0 /var/www/html glusterfs defaults,_netdev 0 0" >> /etc/fstab
+cd $HOME
+wget https://wordpress.org/latest.tar.gz
+tar $HOME/xvfz latest.tar.gz
